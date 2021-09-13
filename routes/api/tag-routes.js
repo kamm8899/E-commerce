55cdffbc3b00b4ce4c067d0685b59ac80c34845e
kamm8899 <kamm8899@gmail.com>
2021-09-13 16:59:16 -0700

    added get Tag findOne route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
+//works
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
@@ -25,10 +25,34 @@ router.get('/', (req, res) => {
 
 });
 
-
+//GET ID route
+//works
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where:{
+      id: req.params.id
+    },
+    include:[{
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      model: Product,
+      through: ProductTag, 
+      as: "ProductTag"
+    },
+    ]
+})
+.then(dbTagData =>{
+  if(!dbTagData){
+    res.status(404).json({ message: 'No Tag found with this id'});
+    return;
+  }
+  res.json(dbTagData);
+})
+.catch(err =>{
+  console.log(err);
+  res.status(500).json(err);
+})
 });
 
 router.post('/', (req, res) => {
