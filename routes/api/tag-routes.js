c511f94b1007f2f6b33bd3d5164d7ae072e79373
kamm8899 <kamm8899@gmail.com>
2021-09-13 17:37:51 -0700

    added delete route for Tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -98,6 +98,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+    id: req.params.id
+    }
+  })
+.then(dbTagData =>{
+  if(!dbTagData){
+    res.status(404).json({message: 'No Tag found with the tag ID'});
+  }
+  res.json(dbTagData);
+})
+.catch(err =>{
+  console.log(err);
+  res.status(500).json(err);
+})
 });
 
 module.exports = router;
